d534df43d2793bd6c3f068f7ef1ac52407c5d670
borris starodubtsev <boris.starodubtsev@adorsis.com>
2023-05-11 10:36:14 +0200

    WebFlux R2DBC

--- src/main/java/com/example/webfluxexample/PaginationFluxWebFluxClient.java
+++ src/main/java/com/example/webfluxexample/PaginationFluxWebFluxClient.java
@@ -1,21 +1,27 @@
 package com.example.webfluxexample;
 
+import com.example.webfluxexample.domain.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 public class PaginationFluxWebFluxClient {
 
     private static final WebClient webClient = WebClient.builder().build();
 
-    private static final String uriBase = "http://localhost:8080/api/v1/webflux";
+    private static final String uriBase = "http://localhost:8080/api/v1/webflux/r2dbc";
 
 
     public static void main(String[] args) {
 
-        Mono.just("/r2dbc")
-                .flatMap(PaginationFluxWebFluxClient::sendGetRequestAsynchronously)                 // Get Id
+        Message newMessage = new Message();
+
+        sendGetRequestAsynchronously()                 // Get Messages
                 .doOnSubscribe(sub -> log.info("Sending request to WebFluxExampleServer"))
                 .doOnSuccess(response -> log.info("Response - \n{}", response))
                 .doOnError(error -> {
@@ -25,12 +31,27 @@ public class PaginationFluxWebFluxClient {
 
     }
 
-    private static Mono<String> sendGetRequestAsynchronously(String subRequest) {
+    private static Mono<List<Message>> sendGetRequestAsynchronously() {
 
         return webClient
                 .get()
-                .uri(uriBase + subRequest)
+                .uri(uriBase)
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(new ParameterizedTypeReference<>() {
+                });
+    }
+
+    private static Mono<Void> sendPostRequestAsynchronously(Message body) {
+
+        return webClient
+                .post()
+                .uri(uriBase)
+                .body(BodyInserters.fromValue(body))
+                .exchangeToMono(new Void.class);
+    }
+
+    private static Mono<String> sendNextMessage(List<Message> messages) {
+        long lastId = messages.stream().reduce((first, last) -> last).orElse(new Message(0L,"")).getId();
+        return sendPostRequestAsynchronously(new Message(lastId+1,"New Data");
     }
 }
---
+++ src/main/java/com/example/webfluxexample/domain/Message.java
@@ -0,0 +1,13 @@
+package com.example.webfluxexample.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private Long id;
+    private String data;
+}
