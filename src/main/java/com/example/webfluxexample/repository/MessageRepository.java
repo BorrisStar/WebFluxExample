9976e099d4ffb5abb4cfe1d84a02db5e2cc04b3a
borris starodubtsev <boris.starodubtsev@adorsis.com>
2023-05-11 09:30:14 +0200

    WebFlux R2DBC

--- src/main/java/com/example/webfluxexample/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.webfluxexample.repository;
-
-import com.example.webfluxexample.domain.Message;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface MessageRepository extends ReactiveCrudRepository<Message, Long> {
-}
